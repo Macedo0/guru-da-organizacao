9af91d8a153540cae5aa2460d8fa8c1c1a88ff3b
Camila Macedo <104233522+Macedo0@users.noreply.github.com>
2022-11-10 22:53:05 -0300

    Update Aplicacao.java

--- src/main/java/app/Aplicacao.java
+++ src/main/java/app/Aplicacao.java
@@ -14,8 +14,6 @@ public class Aplicacao {
     public static void main(String[] args) {
         port(6789);
         
-        staticFiles.location("/public");
-        
         post("/cadastrar/usuario", (request, response) -> usuarioService.add(request, response));
 
         post("/login", (request, response) -> usuarioService.login(request, response));
